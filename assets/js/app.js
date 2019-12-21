270783b32a49fcebeb366438d246cfe126edb24b
Pamela Vasquez <pamelavasquez94@gmail.com>
2019-12-20 21:44:44 -0500

    DOCS: added firebase config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-alert("hello world");
+// Your web app's Firebase configuration
+var config = {
+  apiKey: "AIzaSyAtW7cmJuMurho2WrqqH5-QNjN1iWPrJf8",
+  authDomain: "multiplayer-rps-5bd40.firebaseapp.com",
+  databaseURL: "https://multiplayer-rps-5bd40.firebaseio.com",
+  projectId: "multiplayer-rps-5bd40",
+  storageBucket: "multiplayer-rps-5bd40.appspot.com",
+  messagingSenderId: "551982006270",
+  appId: "1:551982006270:web:68eaf9b4de048f2a150aaf"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+var database = firebase.database();
